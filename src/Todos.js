34073f085cb3e07e8cd766b5a515f62501612696
justDimas <dmitriy.smal@mail.ru>
2023-10-22 23:33:24 +0300

    initial commit

---
+++ src/Todos.js
@@ -0,0 +1,21 @@
+let todos = [
+	{
+		id: 1,
+		title: 'Learn html',
+		finished: true
+	},{
+		id: 2,
+		title: 'Learn css',
+		finished: true
+	},{
+		id: 3,
+		title: 'Learn js',
+		finished: true
+	},{
+		id: 4,
+		title: 'Find job',
+		finished: true
+	}
+]
+
+export const fetchTodos = () => todos
